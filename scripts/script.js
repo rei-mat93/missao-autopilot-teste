7779c4e46f3381e09953a677257750b5a925d83d
rei-mat93 <mateusmachowaky@gmail.com>
2025-10-12 20:11:11 -0300

    Ajuste e Incremento
    Ajuste na responsividade, para se adequar a tela 1366x768 e adição da sessão genius

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
-const binarios = ['01110011 01101111 01100110 01110100 01110111 01100001 01110010 01100101', '01110000 01111001 01110100 01101000 01101111 01101110', '01100100 01100001 01110100 01100001 01110011 01100011 01101001 01100101 01101110 01100011 01100101', '01100110 01110010 01100001 01101101 01100101 01110111 01101111 01110010 01101011']
-const respostaBinarios = ['software', 'python', 'datascience', 'framework']
+const binarios = ['01110011 01101111 01100110 01110100 01110111 01100001 01110010 01100101', '01110000 01111001 01110100 01101000 01101111 01101110', '01100100 01100001 01110100 01100001 01110011 01100011 01101001 01100101 01101110 01100011 01100101', '01100110 01110010 01100001 01101101 01100101 01110111 01101111 01110010 01101011'];
+const respostaBinarios = ['software', 'python', 'datascience', 'framework'];
 
-const respostaCifraDeCesar = ['inteligencia artificial', 'desempenho', 'confiabilidade', 'usabilidade', 'responsividade', 'manutenibilidade']
+const respostaCifraDeCesar = ['inteligencia artificial', 'desempenho', 'confiabilidade', 'usabilidade', 'responsividade', 'manutenibilidade'];
+
+const respostaGenius = [5,5,3,4,1,2,4,3,5,1];
 
 const iniciar = document.getElementById('botao');
 const mudarTela = document.querySelector('body');
@@ -29,7 +31,7 @@ function atualizarTimer() {
         clearInterval(contagem);
         timerElement.textContent = "00:00";
         mudarTela.innerHTML = '';
-        mudarTela.style.backgroundImage = 'url(../imgs/fim.png)'
+        mudarTela.style.backgroundImage = 'url(/imgs/fim.png)'
     }
 
     tempo--;
@@ -39,6 +41,12 @@ function iniciarTimer() {
     if (contagem) return; 
     atualizarTimer();
     contagem = setInterval(atualizarTimer, 1000);
+}
+
+function temporizador() {
+    timerElement = document.querySelector('.timer');
+    atualizarTimer();
+    iniciarTimer();
 }
 
 function cifraDeCesar(texto, deslocamento){
@@ -83,15 +91,11 @@ iniciar.addEventListener('click', () => {
                 <img src="imgs/cadeado fechado.png">
                 <img src="imgs/cadeado fechado.png">
                 <img src="imgs/cadeado fechado.png">
-                <img src="imgs/cadeado fechado.png">
-                <img src="imgs/cadeado fechado.png">
             </div>
         </div>
     </main>`;
 
-    timerElement = document.querySelector('.timer');
-    atualizarTimer();
-    iniciarTimer();
+    temporizador();
 
     const senha = document.getElementById('senha');
 
@@ -102,14 +106,14 @@ iniciar.addEventListener('click', () => {
             const senhaCorreta = respostaBinarios[binarioDaVez];
 
             if (senhaInserida === senhaCorreta) {
-                    let cifraValor = sortearNumero(1, 26);
+                    let cifraValor = sortearNumero(1, 25);
                     let cifraDaVez = sortearNumero(0, respostaCifraDeCesar.length - 1);
                     let palavraCriptografada = respostaCifraDeCesar[cifraDaVez]
 
                     mudarTela.innerHTML = `
                     <main class="display">
                         <div class="desafio">
-                            <p class="desafio-titulo">3. Cifra de Cesar</p>
+                            <p class="desafio-titulo">2. Cifra de Cesar</p>
                             <div class="criptografia">
                                 <h2 class="cifra-valor">${cifraValor}</h2>
                                 <p class="desafio-texto">${cifraDeCesar(palavraCriptografada, cifraValor)}</p>
@@ -125,8 +129,6 @@ iniciar.addEventListener('click', () => {
                                 <img src="imgs/cadeado aberto.png">
                                 <img src="imgs/cadeado aberto.png">
                                 <img src="imgs/cadeado fechado.png">
-                                <img src="imgs/cadeado fechado.png">
-                                <img src="imgs/cadeado fechado.png">
                             </div>
                         </div>
                     </main>`;
@@ -139,13 +141,59 @@ iniciar.addEventListener('click', () => {
 
                         if (senhaInserida === palavraCriptografada) {
                             alert('Parabéns, você concluiu o desafio!');
+
+                            mudarTela.innerHTML = `
+                                <main class="display">
+                                    <div class="desafio">
+                                        <p class="desafio-titulo">3. Genius</p>
+                                        <div class="quadrados">
+                                            <div class="quadrado" data-id="1"></div>
+                                            <div class="quadrado" data-id="2"></div>
+                                            <div class="quadrado" data-id="3"></div>
+                                            <div class="quadrado" data-id="4"></div>
+                                            <div class="quadrado" data-id="5"></div>
+                                        </div>
+                                    </div>
+                                    <div class="cronometro">
+                                        <p class="timer"></p>
+                                        <div class="nivel">
+                                            <img src="imgs/cadeado aberto.png">
+                                            <img src="imgs/cadeado aberto.png">
+                                            <img src="imgs/cadeado fechado.png">
+                                        </div>
+                                    </div>
+                                </main>`;
+
+                            let ordemClique = [];
+                            const quadrados = document.querySelectorAll('.quadrado');
+
+                            quadrados.forEach(quadrado => {
+                                quadrado.addEventListener('click', () => {
+                                    const id = parseInt(quadrado.dataset.id);
+                                    ordemClique.push(id);
+
+                                    const index = ordemClique.length - 1;
+
+                                    if (ordemClique[index] !== respostaGenius[index]) {
+                                        quadrados.forEach (el  => el.classList.add('errou'));
+                                        setTimeout(() => {
+                                            quadrados.forEach (el  => el.classList.remove('errou'));
+                                            ordemClique = [];
+                                        }, 1000);
+                                        return;
+                                    }
+
+                                    if (ordemClique.length === respostaGenius.length) {
+                                        alert('Parabéns, você concluiu o desafio!');
+                                    }   
+                                });
+                            temporizador();
+                            });
                         }
                         }
                     });
 
-                timerElement = document.querySelector('.timer');
-                atualizarTimer();
-                iniciarTimer();
+                temporizador();
             }
         }
     })
